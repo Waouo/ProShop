80cc414887a25e0f2cb4b9958ce529d3eac6c303
waouo <f5864565@gmail.com>
2020-11-27 13:41:01 +0800

    'init-update'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 function App() {
   return (
     <>
-      <h2>Om</h2>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen/>
+        </Container>
+      </main>
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
